7f328ce87bdc1ee8923dea9d727829fc129d9865
Ignacio Zamo <ignaciozamo1@gmail.com>
2023-09-19 23:29:39 -0300

    continuo con ejercicio de carga de operarios

--- NachoA/operario/main.js
+++ NachoA/operario/main.js
@@ -1,9 +1,14 @@
 const root = document.getElementById("root");
 
+const contenedorTitulo = document.createElement("div");
+contenedorTitulo.classList.add("contenedorTitulo");
+
 const titulo = document.createElement("h1");
 titulo.innerText="Operarios - Capitel";
+titulo.classList.add("titulo");
 
-root.appendChild(titulo);
+contenedorTitulo.appendChild(titulo)
+root.appendChild(contenedorTitulo);
 
 const contenedorGeneral = document.createElement("div")
 
@@ -23,14 +28,42 @@ const trabajadores = [
         telefono: "3425218567",
         puesto: "Administrativo A",
         foto: "https://previews.123rf.com/images/grgroup/grgroup1610/grgroup161001846/64251039-icono-de-la-mujer-de-dibujos-animados-persona-de-la-gente-de-avatar-y-tema-humano-dise%C3%B1o-aislado.jpg"
+    },
+    {
+        nombre:"Flores Pamelangas",
+        dni:"35769987",
+        fechaNac:"07/10/1991",
+        telefono: "3425664740",
+        puesto: "Administrativo C",
+        foto: "https://pm1.aminoapps.com/6727/2094cba9ad102b067bb2111df5cf127a6b968b43v2_00.jpg"
     }
+
    
 ]
 
-const fichasTrabajadores = document.createElement("div");
+function agregarTrabajador(nombre, dni, fechaNac, telefono, puesto, foto) {
 
+    const nuevoTrabajador = {
+        nombre: nombre,
+        dni: dni,
+        fechaNac: fechaNac,
+        telefono: telefono,
+        puesto: puesto,
+        foto: foto
+    };
+    
+    trabajadores.push(nuevoTrabajador);
+}
+
+
+const fichasTrabajadores = document.createElement("div");
+fichasTrabajadores.classList.add("pizarra");
+
+
+const agregarOperarioFicha = ()=>{
 trabajadores.forEach(trabajador => {
     fichaTrabajador = document.createElement("div");
+    fichaTrabajador.classList.add("contenedorOperario");
 
     const nombreElemento = document.createElement("p");
     nombreElemento.innerText = `Nombre: ${trabajador.nombre}`;
@@ -43,6 +76,7 @@ trabajadores.forEach(trabajador => {
     const puestoElemento = document.createElement("p");
     puestoElemento.innerText = `Puesto: ${trabajador.puesto}`;
     const fotoElemento = document.createElement("img");
+    fotoElemento.classList.add("foto")
     fotoElemento.src = trabajador.foto;
 
 
@@ -57,51 +91,17 @@ fichasTrabajadores.appendChild(fichaTrabajador);
 
 
 })
+}
+
+
+
+agregarTrabajador("Nuevo Trabajador", "12345678", "01/01/1990", "1234567890", "Nuevo Puesto", "https://imgs.search.brave.com/IHtJdCP-zDqem_uiseeSEVFwA_s4Fa6jLjsQcefI5T4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/bmF0aW9uYWxnZW9n/cmFwaGljLmNvbS5l/cy9tZWRpby8yMDIz/LzA2LzAyL29zby1w/aXJlbmFpY28tbnVl/dmEtMjUtYW5pdmVy/c2FyaW9fMDAwMDAw/MDBfYmRjNjc3NTZf/MjMwNjAyMTI0ODQx/XzgwMHg4MDAuanBn");
+
+agregarOperarioFicha();
+
+
+
+
 
 root.appendChild(fichasTrabajadores);
 
-
-
-
-class NodoCreation {
-    constructor(nombre,dni,fechaNac,telefono,puesto, foto){
-        this.nombre = nombre;
-        this.dni = dni;
-        this.fechaNac = fechaNac;
-        this.telefono = telefono;
-        this.puesto = puesto;
-        this.foto = foto;
-
-    }
-
-    creadorOperario() {
-        const contenedorOperario = document.createElement("div");
-
-        const nombreOperario = document.createElement("b");
-        const dniOperario = document.createElement("p");
-        const fechaNOperario = document.createElement("p");
-        const telefonoOperario = document.createElement("p");
-        const puestoOperario = document.createElement("p");
-        const fotoOperario = document.createElement("img");
-
-        nombreOperario.innerText = this.nombre;
-        dniOperario.innerText = this.dni;
-        fechaNOperario.innerText = this.fechaNac;
-        telefonoOperario.innerText = this.telefono;
-        puestoOperario.innerText = this.puesto;
-        fotoOperario.src = this.foto;
-
-        contenedorOperario.appendChild(nombreOperario);
-        contenedorOperario.appendChild(dniOperario);
-        contenedorOperario.appendChild(fechaNOperario);
-        contenedorOperario.appendChild(telefonoOperario);
-        contenedorOperario.appendChild(puestoOperario);
-        contenedorOperario.appendChild(fotoOperario);
-
-        return contenedorOperario
-    }
-
-
-
-
-}
